d0171a1c606cc065a7dd01f77799d884a468f629
taowenyin <wenyin.tao@163.com>
2020-11-23 22:25:05 +0800

    添加TCN模型

--- model/TCNModel.py
+++ model/TCNModel.py
@@ -3,11 +3,10 @@ import torch.nn as nn
 from torch.nn.utils import weight_norm
 
 
-# 定义因果卷积
+# 定义剪裁模块用于减去多余的padding
 class Chomp1d(nn.Module):
     def __init__(self, chomp_size):
         super(Chomp1d, self).__init__()
-        # 对继承自父类的属性进行初始化
         self.chomp_size = chomp_size
 
     def forward(self, x):
@@ -17,6 +16,9 @@ class Chomp1d(nn.Module):
 # 定义残差模块
 class TemporalBlock(nn.Module):
     """
+    相当于一个Residual block
+
+    输入数据形状：(N, C, L)，N表示batch_size，C表示通道数即数据维度，L表示数据长度
     n_inputs: 输入通道
     n_outputs: 输出通道
     kernel_size: 卷积核大小
@@ -27,10 +29,11 @@ class TemporalBlock(nn.Module):
     """
     def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
         super(TemporalBlock, self).__init__()
-        # 归一化模型的参数
+        # 定义残差模块的第一层扩张卷积
+        # 经过conv，输出的size为(Batch, input_channel, seq_len + padding)，并归一化模型的参数
         self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
                                            stride=stride, padding=padding, dilation=dilation))
-        # 定义残差模块的第一层扩张卷积
+        # 裁剪掉多出来的padding部分，维持输出时间步为seq_len
         self.chomp1 = Chomp1d(padding)
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
@@ -60,6 +63,9 @@ class TemporalBlock(nn.Module):
             self.downsample.weight.data.normal_(0, 0.01)
 
     def forward(self, x):
+        """
+        :param x: size of (Batch, input_channel, seq_len)
+        """
         out = self.net(x)
         res = x if self.downsample is None else self.downsample(x)
         # 残差模块
@@ -69,8 +75,8 @@ class TemporalBlock(nn.Module):
 # 定义TCN架构
 class TemporalConvNet(nn.Module):
     """
-    num_inputs:
-    num_channels: 各层卷积运算的输出通道数或卷积核的数量，该长度即需要执行的卷基层数量
+    num_inputs: 输入通道数
+    num_channels: 每层的hidden_channel数，例如[25,25,25,25]表示有4个隐层，每层hidden_channel数为25
     """
     def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
         super(TemporalConvNet, self).__init__()
@@ -79,8 +85,9 @@ class TemporalConvNet(nn.Module):
         for i in range(num_levels):
             # 扩张系数随着网络层级的增加而呈现指数级增加，以此来增大感受野
             dilation_size = 2 ** i
-            # 计算输入输出通道数
+            # 计算输入通道数
             in_channels = num_inputs if i == 0 else num_channels[i-1]
+            # 确定每一层的输出通道数
             out_channels = num_channels[i]
             # 从num_channels中抽取每个残差模块的输入通道数和输出通道数
             layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
@@ -90,4 +97,12 @@ class TemporalConvNet(nn.Module):
         self.network = nn.Sequential(*layers)
 
     def forward(self, x):
+        """
+        输入x的结构不同于RNN，一般RNN的size为(Batch, seq_len, channels)或者(seq_len, Batch, channels)，
+        这里把seq_len放在channels后面，把所有时间步的数据拼起来，当做Conv1d的输入尺寸，实现卷积跨时间步的操作，
+        很巧妙的设计。
+
+        :param x: size of (Batch, input_channel, seq_len)
+        :return: size of (Batch, output_channel, seq_len)
+        """
         return self.network(x)
